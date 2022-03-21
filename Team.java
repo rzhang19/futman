f650e9a0d4b6dbea2a837c4711e7df1b00aa18c5
Robin Zhang <rzhang19@users.noreply.github.com>
2022-03-21 02:18:54 -0400

    Update Team.java

--- Team.java
+++ Team.java
@@ -1,9 +1,15 @@
 public class Team {
    private static int ID_COUNTER = 1;
    
+   // Identifiable values
    private int m_ID;
    private String m_longName;
    private String m_shortName;
+   
+   // Seasonal values
+   private int m_seasonWins = 0;
+   private int m_seasonDraws = 0;
+   private int m_seasonLosses = 0;
    
    public Team() {
       this ("", "");
