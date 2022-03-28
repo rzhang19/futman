294e65ad620d150779917811933d2c9f0cda297b
rzhang1997 <robinwzh@gmail.com>
2022-03-28 12:53:50 -0400

    FUTMAN-56_2
    Add equals() for the Season class

--- Season.java
+++ Season.java
@@ -1,10 +1,24 @@
 public class Season {
+   private static int ID_COUNTER = 1;
+   
+   // identifier
+   private int m_id;
+   
    private int m_year;
    
    private boolean m_completed;
    
    public Season() {
-      
+      m_id = ID_COUNTER;
+      ID_COUNTER++;
+   }
+   
+   public boolean equals(Season other) {
+      return m_id == other.getID();
+   }
+   
+   public int getID() {
+      return m_id;
    }
    
    public int getYear() {
