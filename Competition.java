d4bd562ffe0be2ab5a82c8a6085b1e4561c3a7d5
rzhang1997 <robinwzh@gmail.com>
2022-03-28 01:06:15 -0400

    FUTMAN-53
    Add details for Season class

--- Competition.java
+++ Competition.java
@@ -1,6 +1,8 @@
 public class Competition {
    private static int ID_COUNTER = 1;
    
+   private static int m_currentYear = 2022;
+   
    public Competition() {
       
    }
--- Season.java
+++ Season.java
@@ -1,5 +1,21 @@
 public class Season {
+   private static int ID_COUNTER = 1;
+   
+   private int m_id;
+   
+   private int m_year;
+   
+   private boolean m_completed;
+   
    public Season() {
       
+   }
+   
+   public int getYear() {
+      return m_year;
+   }
+   
+   public boolean getCompleted() {
+      return m_completed;
    }
 }
