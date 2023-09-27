3e25e898e8df1c392728f8f8c39d19eb08f4f2a6
rzhang1997 <robinwzh@gmail.com>
2023-09-27 00:07:01 -0700

    FUTMAN-16
    Create override for RunMatch for Cup Match

--- com/futman/src/test/CupMatchTest.java
+++ com/futman/src/test/CupMatchTest.java
@@ -1,17 +1,25 @@
 package com.futman.src.test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.futman.src.main.Country;
+import com.futman.src.main.CupMatch;
+import com.futman.src.main.Match;
+import com.futman.src.main.Team;
+
 class CupMatchTest {
+	static Country country = new Country("United States", "USA");
+	static Team team1 = new Team("Team1", "T1", country);
+	static Team team2 = new Team("Team2", "T2", country);
+	static Match cupMatch;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		cupMatch = new CupMatch(team1, team2, country);
 	}
 
 	@AfterAll
@@ -27,8 +35,8 @@ class CupMatchTest {
 	}
 
 	@Test
-	void test() {
-		assert true;
+	void test_runMatch() {
+		assert cupMatch.runMatch();
 	}
 
 }
